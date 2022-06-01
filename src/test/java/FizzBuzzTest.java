c367e0cf9990765a10c7cb168b2bd7655f5f3658
tedbeast <tedbalashov@gmail.com>
2022-06-01 09:32:58 -0400

    fixed fizzbuzztest

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -10,26 +10,31 @@ public class FizzBuzzTest {
         String target = "Buzz";
         Assert.assertEquals(target, fb.FizzBuzzGen(num));
     }
+    @Test
     public void FizzBuzzTest2(){
         int num = 9;
         String target = "Fizz";
         Assert.assertEquals(target, fb.FizzBuzzGen(num));
     }
+    @Test
     public void FizzBuzzTest3(){
         int num = 27;
         String target = "Fizz";
         Assert.assertEquals(target, fb.FizzBuzzGen(num));
     }
+    @Test
     public void FizzBuzzTest4(){
         int num = 30;
         String target = "FizzBuzz";
         Assert.assertEquals(target, fb.FizzBuzzGen(num));
     }
+    @Test
     public void FizzBuzzTest5(){
         int num = 28;
         String target = "";
         Assert.assertEquals(target, fb.FizzBuzzGen(num));
     }
+    @Test
     public void FizzBuzzTest6(){
         int num = 2;
         String target = "";
