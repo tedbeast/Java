d6aa508dd06ebb52a89764d936d5250defdb0a5a
tedbeast <tedbalashov@gmail.com>
2022-07-08 00:32:04 -0400

    added more problem

---
+++ src/main/java/Level0/GetMax/GetMax.java
@@ -0,0 +1,7 @@
+package Level0.GetMax;
+//get max int from array
+public class GetMax {
+    public int max(int[] arr){
+        return -1;
+    }
+}
---
+++ src/main/java/Level1/IsThereSum/IsThereSum.java
@@ -0,0 +1,10 @@
+package Level1.IsThereSum;
+/*
+return true if two separate values in the array add up to a target.
+false otherwise.
+ */
+public class IsThereSum {
+    public boolean check(int[] arr, int target){
+        return false;
+    }
+}
